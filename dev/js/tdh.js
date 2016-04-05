c0b0e68250be68bf549f68396202c238a63a140b
Obeida Shamon <oshamoun@students.uni-mainz.de>
2016-04-05 15:30:33 +0200

    Accordion: implement accordion element in javascript

--- dev/js/tdh.js
+++ dev/js/tdh.js
@@ -1,1 +1,19 @@
 /* individuelles JS */
+
+$(document).ready(function () {
+
+  // Code zum Ein- und Ausklappen der Akkordionliste
+  $('.activities_left h3').each( function (){
+    $(this).parent().toggleClass('suppressed');
+      $( this ).click( function (){
+        $(this).parent().toggleClass('suppressed');
+        var sibs = $(this).parent().siblings().each(
+          function () {
+        $(this).addClass('suppressed');
+        })
+      } );
+    });
+
+    // schreib deinen Code nach diesem Kommentar
+
+});
