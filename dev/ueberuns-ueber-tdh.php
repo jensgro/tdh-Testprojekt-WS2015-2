16fb9ad1a60013614416bdc34b2af887d9cefb5c
Jens Grochtdreis <jens@grochtdreis.de>
2016-04-06 19:38:24 +0200

    Weitere Mitarbeiter-Tabelle hinzugefügt und neue Seite angelegt

---
+++ dev/ueberuns-ueber-tdh.php
@@ -0,0 +1,19 @@
+﻿<?php
+  $pagetitle= "Das Team - terre des hommes Murgtal/Mittelbaden";
+
+	$navigation = "uebertdh";
+
+	$contentleftincludes = array(
+    'employee_x.inc'
+	);
+
+	$contentrightincludes = array(
+    'colored-wrapper-name-list.inc'
+	);
+
+	$sideincludes = array(
+		'leer.inc'
+	);
+
+	include 'inc/templates/template-2-1.inc';
+?>
