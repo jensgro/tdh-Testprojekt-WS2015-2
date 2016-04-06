0b9848c436fd6e5e7a77419be27f7e182b78bc88
Jens Grochtdreis <jens@grochtdreis.de>
2016-04-06 19:33:25 +0200

    Eine Mitarbeitertabelle hinzugefügt und eine neue Seite dafür erstellt und verlinkt. Kommentar ins CSS geschrieben.

---
+++ dev/ueberuns-mitwirkende.php
@@ -0,0 +1,19 @@
+﻿<?php
+  $pagetitle= "Das Team - terre des hommes Murgtal/Mittelbaden";
+
+	$navigation = "mitwirkende";
+
+	$contentleftincludes = array(
+    'table-employee-jsiekier.inc'
+	);
+
+	$contentrightincludes = array(
+    'colored-wrapper-name-list.inc'
+	);
+
+	$sideincludes = array(
+		'leer.inc'
+	);
+
+	include 'inc/templates/template-2-1.inc';
+?>
