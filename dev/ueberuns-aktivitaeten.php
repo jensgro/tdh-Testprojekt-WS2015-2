52386105969d14e833515f4aefe045f612b21c05
Jens Grochtdreis <jens@grochtdreis.de>
2016-04-06 14:06:42 +0200

    Bei den Aktivitäten ein wenig Dummy-Content hinzugefügt und Quellcode netter formatiert

--- dev/ueberuns-aktivitaeten.php
+++ dev/ueberuns-aktivitaeten.php
@@ -7,9 +7,18 @@
 
 	$contentleftincludes = array(
     'activities/contentleft_header.inc',
-    'activities/contentleft_category_header.inc','activities/contentleft_category_year_detailed.inc','activities/contentleft_category_year_short.inc','activities/contentleft_category_footer.inc',
-    'activities/contentleft_category_header.inc','activities/contentleft_category_year_detailed.inc','activities/contentleft_category_year_short.inc','activities/contentleft_category_footer.inc',
-    'activities/contentleft_category_header.inc','activities/contentleft_category_year_detailed.inc','activities/contentleft_category_year_short.inc','activities/contentleft_category_footer.inc',
+    'activities/contentleft_category_header.inc',
+    'activities/contentleft_category_year_detailed.inc',
+    'activities/contentleft_category_year_short.inc',
+    'activities/contentleft_category_footer.inc',
+    'activities/contentleft_category_header.inc',
+    'activities/contentleft_category_year_detailed.inc',
+    'activities/contentleft_category_year_short.inc',
+    'activities/contentleft_category_footer.inc',
+    'activities/contentleft_category_header.inc',
+    'activities/contentleft_category_year_detailed.inc',
+    'activities/contentleft_category_year_short.inc',
+    'activities/contentleft_category_footer.inc',
     'activities/contentleft_footer.inc'
 	);
 
